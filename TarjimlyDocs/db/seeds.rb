c26ca0d74ff341fd785b74e5660c89305bdaa1e4
Cassidy Hardin <cassidyhardin@berkeley.edu>
2019-11-06 21:13:10 -0800

    initialize views

--- TarjimlyDocs/db/seeds.rb
+++ TarjimlyDocs/db/seeds.rb
@@ -10,9 +10,9 @@ requests_seed = [{:user_tarjimly_id => 1, :from_language => "English", :to_langu
             {:user_tarjimly_id => 1, :from_language => "English", :to_language => "Urdu", :document => "location_of_file", :document_format => "pdf", :deadline  => "25-Nov-2019",
             :title => "Camp Announcment", :description => "Event going on in camp", :categories => ['Refugee Camp', 'Event'], :num_claims => 0, :form_type => "N/A", :_status => 0},
             {:user_tarjimly_id => 2, :from_language => "Arabic", :to_language => "Urdu", :document => "location_of_file", :document_format => "jpg", :deadline  => "25-Dec-2019",
-            :title => "Visa Application", :description => "Information for steps in visa application", :categories => ['Visa', 'Legal'], :num_claims => 0, :form_type => "Visa Application", :_status => 0}
+            :title => "Visa Application", :description => "Information for steps in visa application", :categories => ['Visa', 'Legal'], :num_claims => 0, :form_type => "Visa Application", :_status => 0},
             {:user_tarjimly_id => 3, :from_language => "Urdu", :to_language => "English", :document => "location_of_file", :document_format => "png", :deadline  => "15-Nov-2019",
-            :title => "Governement Forms", :description => "Governemnt forms regarding family dependencies", :categories => ['Government', 'Forms'], :num_claims => 0, :form_type => "N/A", :_status => 0}
+            :title => "Governement Forms", :description => "Governemnt forms regarding family dependencies", :categories => ['Government', 'Forms'], :num_claims => 0, :form_type => "N/A", :_status => 0},
             {:user_tarjimly_id => 4, :from_language => "English", :to_language => "Arabic", :document => "location_of_file", :document_format => "pdf", :deadline  => "01-Jan-2020",
             :title => "Doctor Notes", :description => "Notes from recent Doctors visit", :categories => [], :num_claims => 0}, ,:form_type => "N/A", :_status => 0},
             {:user_tarjimly_id => 5, :from_language => "English", :to_language => "Arabic", :document => "location_of_file", :document_format => "pdf", :deadline  => "25-Dec-2019",
