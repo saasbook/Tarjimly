ffff029fca776bf04ff908eb28aa102507c68b68
Cassidy Hardin <cassidyhardin@Cassidys-MacBook-Pro-2.local>
2019-11-09 02:22:56 -0800

    delete cucumber tests

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,41 @@
+Given /the following requests exist/ do |requests_table|
+    requests_table.hashes.each do |r|
+      Request.create r
+    end
+  end
+
+#   Given("I am on my {string} page") do |string|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+
+#   When("I am on my {string} page") do |string|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+
+#   Then("I should see {string} and {string}") do |string, string2|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+
+#   Then("I click {string}") do |string|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+
+#   Then("I should see a list of {requests} sorted by {category}") do |requests, category|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+
+#   When("I go to the edit page for {request}") do |request|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+
+#   Then("I should see {string}") do |string|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+  
+#   When("I click {string}") do |string|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
+  
+#   Then("I should not see {string}") do |string|
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
