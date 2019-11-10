9aca83e51066206d238b248ffdc6e9dba54ae48e
Ahmad Jawaid <ahmadjawaid@berkeley.edu>
2019-11-10 01:09:12 -0800

    fixed routes and seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-requests_seed = [{:user_tarjimly_id => 1, :from_language => "English", :to_language => "Arabic", :document => "location_of_file", :document_format => "pdf", :deadline  => "25-Nov-2019",
-            :title => "Camp Announcment", :description => "Event going on in camp", :categories => "MISSING", :num_claims => 0, :form_type => "N/A", :_status => 0},
-            {:user_tarjimly_id => 1, :from_language => "English", :to_language => "Urdu", :document => "location_of_file", :document_format => "pdf", :deadline  => "25-Nov-2019",
-            :title => "Camp Announcment", :description => "Event going on in camp", :categories => "MISSING", :num_claims => 0, :form_type => "N/A", :_status => 0},
-            {:user_tarjimly_id => 2, :from_language => "Arabic", :to_language => "Urdu", :document => "location_of_file", :document_format => "jpg", :deadline  => "25-Dec-2019",
-            :title => "Visa Application", :description => "Information for steps in visa application", :categories => "MISSING", :num_claims => 0, :form_type => "Visa Application", :_status => 0},
-            {:user_tarjimly_id => 3, :from_language => "Urdu", :to_language => "English", :document => "location_of_file", :document_format => "png", :deadline  => "15-Nov-2019",
-            :title => "Governement Forms", :description => "Governemnt forms regarding family dependencies", :categories => "MISSING", :num_claims => 0, :form_type => "N/A", :_status => 0},
-            {:user_tarjimly_id => 4, :from_language => "English", :to_language => "Arabic", :document => "location_of_file", :document_format => "pdf", :deadline  => "01-Jan-2020",
-            :title => "Doctor Notes", :description => "Notes from recent Doctors visit", :categories => "MISSING", :num_claims => 0, :form_type => "N/A", :_status => 0},
-            {:user_tarjimly_id => 5, :from_language => "English", :to_language => "Arabic", :document => "location_of_file", :document_format => "pdf", :deadline  => "25-Dec-2019",
-            :title => "Lawyer Meeting", :description => "Times to schedulue upcoming appointment", :categories => "MISSING", :num_claims => 0, :form_type => "N/A", :_status => 0}
-        ]
+requests_seed = [{user_tarjimly_id: 1, from_language: 'English', to_language: 'Arabic', document: 'location_of_file', document_format: 'pdf', deadline: '25-Nov-2019',
+                  title: 'Camp Announcment', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                 {user_tarjimly_id: 1, from_language: 'English', to_language: 'Urdu', document: 'location_of_file', document_format: 'pdf', deadline: '25-Nov-2019',
+                  title: 'Camp Announcment', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                 {user_tarjimly_id: 2, from_language: 'Arabic', to_language: 'Urdu', document: 'location_of_file', document_format: 'jpg', deadline: '25-Dec-2019',
+                  title: 'Visa Application', description: 'Information for steps in visa application', categories: 'MISSING', num_claims: 0, form_type: 'Visa Application', _status: 0},
+                 {user_tarjimly_id: 3, from_language: 'Urdu', to_language: 'English', document: 'location_of_file', document_format: 'png', deadline: '15-Nov-2019',
+                  title: 'Governement Forms', description: 'Governemnt forms regarding family dependencies', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                 {user_tarjimly_id: 4, from_language: 'English', to_language: 'Arabic', document: 'location_of_file', document_format: 'pdf', deadline: '01-Jan-2020',
+                  title: 'Doctor Notes', description: 'Notes from recent Doctors visit', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                 {user_tarjimly_id: 5, from_language: 'English', to_language: 'Arabic', document: 'location_of_file', document_format: 'pdf', deadline: '25-Dec-2019',
+                  title: 'Lawyer Meeting', description: 'Times to schedulue upcoming appointment', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0}
+]
 
-claims_seed = [{:translator_tarjimly_id => 1, :_status => 0, :submitted_date => "N/A", :translation => "location_of_file", :translation_type => "pdf"},
-            {:translator_tarjimly_id => 3, :_status => 0, :submitted_date => "6-Nov-2019", :translation => "location_of_file", :translation_type => "text-box"},
-            {:translator_tarjimly_id => 3, :_status => 1, :submitted_date => "N/A", :translation => "location_of_file", :translation_type => "pdf"},
-            {:translator_tarjimly_id => 2, :_status => 2, :submitted_date => "N/A", :translation => "location_of_file", :translation_type => "jpg"},
-            {:translator_tarjimly_id => 5, :_status => 0, :submitted_date => "N/A", :translation => "location_of_file", :translation_type => "pdf"}
+claims_seed = [{ translator_tarjimly_id: 1, _status: 0, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf' },
+               {translator_tarjimly_id: 3, _status: 0, submitted_date: '6-Nov-2019', translation: 'location_of_file', translation_type: 'text-box'},
+               {translator_tarjimly_id: 3, _status: 1, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf'},
+               {translator_tarjimly_id: 2, _status: 2, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'jpg'},
+               {translator_tarjimly_id: 5, _status: 0, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf'}
 ]
 
 
-
-
-
 requests_seed.each do |r|
-    Request.create!(r)
+  Request.create(r)
 end
 
 claims_seed.each do |c|
-    Claim.create!(c)
+  Claim.create(c)
 end
