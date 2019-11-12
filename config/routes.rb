9ffaa0f4a45d4a66a2acf87b9de19da4925a7159
Cassidy Hardin <cassidyhardin@berkeley.edu>
2019-11-11 20:34:37 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,6 @@ Rails.application.routes.draw do
   delete '/users/:uid/requests/:request_id' => 'requests#delete', :as => 'delete_request'
 
 
-<<<<<<< HEAD
-  get   '/requests/:uid'             => 'requests#requests',    :as => 'user_requests'
-  get   '/requests/:uid/pending'     => 'requests#pending',     :as => 'user_pending'
-  get   '/requests/:uid/completed'   => 'requests#completed',   :as => 'user_completed'
-  get   '/requests/:uid/:rid'        => 'requests#request',     :as => 'user_request'
-  get   '/requests/:uid/:rid/edit'   => 'requests#edit',        :as => 'edit_request'
-  get   '/requests/:uid/new'         => 'requests#new',         :as => 'new_request'
-  post  '/requests/:uid'             => 'requests#create',      :as => 'create_request'
-  delete'/requests/:uid/new'         => 'requests#delete',      :as => 'delete_request'
-
-
-  get   '/claims/:tid'           => 'claims#claims',      :as => 'translator_claims'
-  get   '/claims/:tid/:cid'      => 'claims#claim',       :as => 'translator_claim'
-  post  '/claims/:tid'           => 'claims#new',         :as => 'new_claim'
-  delete'/claims/:tid/:cid'      => 'claims#delete',      :as => 'delete_claim'
-  patch '/claims/:tid/:cid'      => 'claims#complete',    :as => 'complete_claim'
-  get    '/claims/requests'      => 'claims#requests',    :as => 'translator_requests'
-  get    '/claims/requests/:rid' => 'claims#request',     :as => 'preview_request'
-
-  root 'application#login'
-=======
   get '/translators/requests' => 'claims#requests', :as => 'view_requests'
   get '/translators/requests/:request_id' => 'claims#preview', :as => 'preview_request'
   get '/translators/:tid/claims' => 'claims#index', :as => 'index_claims'
@@ -38,6 +17,5 @@ Rails.application.routes.draw do
   patch '/translators/:tid/claims/:claim_id' => 'claims#complete', :as => 'complete_claim'
   
   root 'requests#index'
->>>>>>> 6408f6d962fbe9c9aef8b89677e146d7b67f4a74
 
 end
