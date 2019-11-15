5eae261cac3174794c54c294449c2b4a6e4bd73e
Ahmad Jawaid <ahmadjawaid@berkeley.edu>
2019-11-14 21:41:16 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,26 @@
 Rails.application.routes.draw do
 
-  get '/login' => 'appliscation#login', :as => 'application_login'
-  get '/users/:uid/requests' => 'requests#index', :as => 'index_requests'
-  get '/users/:uid/requests/:request_id' => 'requests#show', :as => 'show_request'
-  get '/users/:uid/requests/new' => 'requests#new', :as => 'new_request'
-  post '/users/:uid/requests' => 'requests#create', :as => 'create_request'
-  delete '/users/:uid/requests/:request_id' => 'requests#delete', :as => 'delete_request'
+  #Login
+  get '/login' => 'application#login', :as => 'application_login'
 
+  #User Pages
+  get '/requests' => 'requests#index', :as => 'index_requests'
+  get '/requests/:request_id' => 'requests#show', :as => 'show_request'
+  get '/requests/new' => 'requests#new', :as => 'new_request'
+  post '/requests' => 'requests#create', :as => 'create_request'
+  delete '/requests/:request_id' => 'requests#delete', :as => 'delete_request'
 
+  #Translator Pages
+  get 'claims' => 'claims#index', :as => 'index_claims'
+  get 'claims/:claim_id' => 'claims#show', :as => 'show_claim'
+  post 'claims' => 'claims#create', :as => 'create_claim'
+  delete 'claims/:claim_id' => 'claims#delete', :as => 'delete_claim'
+  patch 'claims/:claim_id' => 'claims#complete', :as => 'complete_claim'
+  
+  ## Translator -> View All Requests
   get '/translators/requests' => 'claims#requests', :as => 'view_requests'
   get '/translators/requests/:request_id' => 'claims#preview', :as => 'preview_request'
-  get '/translators/:tid/claims' => 'claims#index', :as => 'index_claims'
-  get '/translators/:tid/claims/:claim_id' => 'claims#show', :as => 'show_claim'
-  post '/translators/:tid/claims' => 'claims#create', :as => 'create_claim'
-  delete '/translators/:tid/claims/:claim_id' => 'claims#delete', :as => 'delete_claim'
-  patch '/translators/:tid/claims/:claim_id' => 'claims#complete', :as => 'complete_claim'
+
   
   root 'requests#index'
 
