cd02f42f822ce4249dd57dc2bfcbefa76a60f595
Ahmad Jawaid <ahmadjawaid@berkeley.edu>
2019-11-14 21:42:14 -0800

    fixed claim routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,17 @@ Rails.application.routes.draw do
   delete '/requests/:request_id' => 'requests#delete', :as => 'delete_request'
 
   #Translator Pages
-  get 'claims' => 'claims#index', :as => 'index_claims'
-  get 'claims/:claim_id' => 'claims#show', :as => 'show_claim'
-  post 'claims' => 'claims#create', :as => 'create_claim'
-  delete 'claims/:claim_id' => 'claims#delete', :as => 'delete_claim'
-  patch 'claims/:claim_id' => 'claims#complete', :as => 'complete_claim'
+  get '/claims' => 'claims#index', :as => 'index_claims'
+  get '/claims/:claim_id' => 'claims#show', :as => 'show_claim'
+  post '/claims' => 'claims#create', :as => 'create_claim'
+  delete '/claims/:claim_id' => 'claims#delete', :as => 'delete_claim'
+  patch '/claims/:claim_id' => 'claims#complete', :as => 'complete_claim'
   
   ## Translator -> View All Requests
   get '/translators/requests' => 'claims#requests', :as => 'view_requests'
   get '/translators/requests/:request_id' => 'claims#preview', :as => 'preview_request'
 
-  
+
   root 'requests#index'
 
 end
