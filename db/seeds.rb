d348eaa1ba782506f09c13868d2b831e25b3db8a
Cassidy Hardin <cassidyhardin@berkeley.edu>
2019-11-21 19:31:24 -0800

    broke travis again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 requests_seed = [{user_tarjimly_id: 1, from_language: 'English', to_language: 'Arabic', document: 'location_of_file', document_format: 'pdf', deadline: '25-Nov-2019',
                   title: 'Camp Announcment', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
                  
