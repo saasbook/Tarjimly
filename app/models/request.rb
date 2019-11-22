053b3e31ee773485230a55e7facfd5d4bcbe87fb
Cassidy Hardin <cassidyhardin@berkeley.edu>
2019-11-22 00:11:16 -0800

    self selfieee

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,8 +3,11 @@ class Request < ApplicationRecord
     before_destroy :update_claims, prepend: true
 
     def update_claims 
-        if @request.claims.present?
-            @request._status = 2
+        if !self.claims.nil? && self.claims.present?
+            self._status = 2
+            self.claims.each do |claim|
+                claim._status = 3
+            end
             return false 
         end 
     end 
