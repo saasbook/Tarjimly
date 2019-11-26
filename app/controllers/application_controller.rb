45aa6e5db1f38dc6f600addcdf230ac3486387b4
Cassidy Hardin <cassidyhardin@berkeley.edu>
2019-11-26 09:13:42 -0800

    test 404 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
     #login check filter 
-
-    def not_found
-        raise ActionController::RoutingError.new('Not Found')
-        render :file => "#{RAILS_ROOT}/public/404.html",  :status => 404
-    end
 end
--- spec/requests/show_request_spec.rb
+++ spec/requests/show_request_spec.rb
@@ -38,4 +38,8 @@ describe "User viewing details for a specific request" do
         visit "/requests/#{@curr_id_deleted}"
         page.should have_content("The page you were looking for doesn't exist.")
     end
+    it 'if request was deleted response should be 404' do 
+        visit "/requests/#{@curr_id_deleted}"
+        expect(page.status_code).to eq(404)
+    end
 end
