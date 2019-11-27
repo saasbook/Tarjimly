e1e21f4a5c3d3629e542b1ee14d90911e6871976
Ahmad Jawaid <ahmadjawaid@berkeley.edu>
2019-11-26 16:10:50 -0800

    structuring

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,67 +6,67 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 requests_seed = [{user_tarjimly_id: 1, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '25-Nov-2019',
-                  title: 'Camp Announcment', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                  title: 'Camp Announcment', description: 'Event going on in camp', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 1, from_language: 'English', to_language: 'Urdu', document_text: 'this is the document text', document_format: 'text', deadline: '25-Nov-2019',
-                  title: 'Camp Announcment', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                  title: 'Camp Announcment', description: 'Event going on in camp', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 2, from_language: 'Arabic', to_language: 'Urdu', document_text: 'this is the document text', document_format: 'text', deadline: '25-Dec-2019',
-                  title: 'Visa Application', description: 'Information for steps in visa application', categories: 'MISSING', num_claims: 0, form_type: 'Visa Application', _status: 0},
+                  title: 'Visa Application', description: 'Information for steps in visa application', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'Visa Application', _status: 0},
                  
                   {user_tarjimly_id: 2, from_language: 'Urdu', to_language: 'English', document_text: 'this is the document text', document_format: 'text', deadline: '15-Nov-2019',
-                  title: 'Governement Forms', description: 'Governemnt forms regarding family dependencies', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                  title: 'Governement Forms', description: 'Governemnt forms regarding family dependencies', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 4, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '01-Jan-2020',
-                  title: 'Doctor Notes', description: 'Notes from recent Doctors visit', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                  title: 'Doctor Notes', description: 'Notes from recent Doctors visit', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 5, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '25-Dec-2019',
-                  title: 'Lawyer Meeting', description: 'Times to schedulue upcoming appointment', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                  title: 'Lawyer Meeting', description: 'Times to schedulue upcoming appointment', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 1, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '25-Nov-2019',
-                    title: 'Camp Event', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                    title: 'Camp Event', description: 'Event going on in camp', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 1, from_language: 'English', to_language: 'Urdu', document_text: 'this is the document text', document_format: 'text', deadline: '25-Nov-2019',
-                    title: 'Doctor Appointment', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                    title: 'Doctor Appointment', description: 'Event going on in camp', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 2, from_language: 'Arabic', to_language: 'Urdu', document_text: 'this is the document text', document_format: 'text', deadline: '25-Dec-2019',
-                    title: 'Visa Request', description: 'Information for steps in visa application', categories: 'MISSING', num_claims: 0, form_type: 'Visa Application', _status: 0},
+                    title: 'Visa Request', description: 'Information for steps in visa application', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'Visa Application', _status: 0},
                  
                   {user_tarjimly_id: 2, from_language: 'Urdu', to_language: 'English', document_text: 'this is the document text', document_format: 'text', deadline: '15-Nov-2019',
-                    title: 'Passport Forms', description: 'Governemnt forms regarding family dependencies', categories: 'MISSING', num_claims: 0, form_type: 'Passport Application', _status: 0},
+                    title: 'Passport Forms', description: 'Governemnt forms regarding family dependencies', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'Passport Application', _status: 0},
                  
                   {user_tarjimly_id: 4, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '01-Jan-2020',
-                    title: 'Marriage License', description: 'Notes from recent Doctors visit', categories: 'MISSING', num_claims: 0, form_type: 'Marriage License', _status: 0},
+                    title: 'Marriage License', description: 'Notes from recent Doctors visit', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'Marriage License', _status: 0},
                  
                   {user_tarjimly_id: 5, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '25-Dec-2019',
-                    title: 'University Diploma', description: 'Times to schedulue upcoming appointment', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                    title: 'University Diploma', description: 'Times to schedulue upcoming appointment', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 1, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '25-Nov-2019',
-                      title: 'University Application', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'University Application', _status: 0},
+                      title: 'University Application', description: 'Event going on in camp', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'University Application', _status: 0},
                  
                   {user_tarjimly_id: 1, from_language: 'English', to_language: 'Urdu', document_text: 'this is the document text', document_format: 'text', deadline: '25-Nov-2019',
-                      title: 'Doctor Meeting Confirmation', description: 'Event going on in camp', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                      title: 'Doctor Meeting Confirmation', description: 'Event going on in camp', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 2, from_language: 'Arabic', to_language: 'Urdu', document_text: 'this is the document text', document_format: 'text', deadline: '25-Dec-2019',
-                      title: 'School Enrollment', description: 'Information for steps in visa application', categories: 'MISSING', num_claims: 0, form_type: 'School Enrollment', _status: 0},
+                      title: 'School Enrollment', description: 'Information for steps in visa application', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'School Enrollment', _status: 0},
                  
                   {user_tarjimly_id: 3, from_language: 'Urdu', to_language: 'English', document_text: 'this is the document text', document_format: 'text', deadline: '15-Nov-2019',
-                      title: 'Governement Notice', description: 'Governemnt forms regarding family dependencies', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                      title: 'Governement Notice', description: 'Governemnt forms regarding family dependencies', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 4, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '01-Jan-2020',
-                      title: 'Medical Note', description: 'Notes from recent Doctors visit', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0},
+                      title: 'Medical Note', description: 'Notes from recent Doctors visit', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0},
                  
                   {user_tarjimly_id: 5, from_language: 'English', to_language: 'Arabic', document_text: 'this is the document text', document_format: 'text', deadline: '25-Dec-2019',
-                      title: 'Lawyer Meeting', description: 'Times to schedulue upcoming appointment', categories: 'MISSING', num_claims: 0, form_type: 'N/A', _status: 0}
+                      title: 'Lawyer Meeting', description: 'Times to schedulue upcoming appointment', categories: ['Legal', 'Medical'], num_claims: 0, form_type: 'N/A', _status: 0}
 ]
 
-claims_seed = [{translator_tarjimly_id: 1, _status: 0, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf', request_id: 1},
-               {translator_tarjimly_id: 1, _status: 3, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'text-box', request_id: 5},
-               {translator_tarjimly_id: 1, _status: 1, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf', request_id: 2},
-               {translator_tarjimly_id: 1, _status: 2, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'jpg', request_id: 3},
-               {translator_tarjimly_id: 1, _status: 0, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf', request_id: 4},
-               {translator_tarjimly_id: 1, _status: 3, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf', request_id: 6},
-               {translator_tarjimly_id: 1, _status: 3, submitted_date: 'N/A', translation: 'location_of_file', translation_type: 'pdf', request_id: 7}
+claims_seed = [{translator_tarjimly_id: 1, _status: 0, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 1},
+               {translator_tarjimly_id: 1, _status: 3, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 5},
+               {translator_tarjimly_id: 1, _status: 1, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 2},
+               {translator_tarjimly_id: 1, _status: 2, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 3},
+               {translator_tarjimly_id: 1, _status: 0, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 4},
+               {translator_tarjimly_id: 1, _status: 3, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 6},
+               {translator_tarjimly_id: 1, _status: 3, submitted_date: 'N/A', translation_text: 'example text translation', translation_format: 'text', request_id: 7}
 ]
 
 
