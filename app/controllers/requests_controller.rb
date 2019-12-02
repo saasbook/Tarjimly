da8853f7aeaa245c1e3d5ebae5afba3230d5d7c1
Ahmad Ibrahim <ahmibrahim97@berkeley.edu>
2019-12-01 23:18:51 -0800

    CSS For index pages

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,9 +1,8 @@
 class RequestsController < ActionController::Base
     def index
         @user = 1
-        status = params[:status] || 0
-
-        @requests = Request.where(user_tarjimly_id: @user, _status: status)
+        @status = params[:status] || 0
+        @requests = Request.where(user_tarjimly_id: @user, _status: @status)
     end
 
     def show 
