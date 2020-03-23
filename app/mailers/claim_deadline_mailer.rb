c0c2a646b364c30943300b1e989a0c23dc2de064
cassidyhardin <cassidyhardin@berkeley.edu>
2020-03-23 15:53:41 +0900

    email notifcations for 24hr deadline

--- app/mailers/claim_deadline_mailer.rb
+++ app/mailers/claim_deadline_mailer.rb
@@ -1,10 +1,12 @@
 class ClaimDeadlineMailer < ApplicationMailer
     default from: 'cassihardin@gmail.com'
     def claim_24hr_deadine(claim_id)
+        puts "WE ARE SENDING AN EMAIL YIPPEEEE"
         @claim = Claim.find_by_id(claim_id)
         translator_email = @claim.email
-        @title = Request.find_by_id(@claim.request_id).title
+        @request = Request.find_by_id(@claim.request_id)
         attachments.inline['logo-2.png'] = File.read('app/assets/images/logo-2.png')
-        mail(to: translator_email, subject: "Your claimed request #{@title} is due in 24 Hours!")
+        mail(to: translator_email, subject: "Your claimed request #{@request.title} is due in 24 Hours!")
     end
 end
+xss
--- config/initializers/email_scheduler.rb
+++ config/initializers/email_scheduler.rb
@@ -2,10 +2,7 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1m' do
-    Rails.logger.info "hello, it's #{Time.now}"
-    Rails.logger.flush
-# scheduler.cron '0 0 * * *' do
+scheduler.cron '0 0 * * *' do
     @all_calims = Claim.where(_status: 0)
     @all_calims.select { |c|
         Request.find_by_id(c.request_id).deadline == Date.tomorrow
